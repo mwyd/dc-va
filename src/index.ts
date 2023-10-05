0fe720c795dfa1c6dea6f32d7e48ec850164e247
mwyd <wydrzanin@gmail.com>
2023-10-05 22:43:35 +0200

    fix cs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import "./dotenv";
 import { GTTSConverter } from "./voice-assistant/text-to-speech/gtts-converter";
 
-new GTTSConverter().convert('test');
+new GTTSConverter().convert("test");
--- src/voice-assistant/text-to-speech/gtts-converter.ts
+++ src/voice-assistant/text-to-speech/gtts-converter.ts
@@ -5,12 +5,12 @@ export class GTTSConverter implements TextToSpeechConverter {
   convert(text: string): Promise<string> {
     // TODO output dir as a config variable
     // TODO unique filename (uuid)
-    const file = 'var/test.mp3';
+    const file = "var/test.mp3";
 
     return new Promise((resolve, reject) => {
-      const cmd = spawn('gtts-cli', [`'${text}'`, '--output', file]);
+      const cmd = spawn("gtts-cli", [`'${text}'`, "--output", file]);
 
-      cmd.on('close', (code) => {
+      cmd.on("close", (code) => {
         if (code !== 0) {
           reject(`Command exited with code ${code}`);
         }
